5ff873ff7d01365c6059e689919bc219231556e0
galzamora <gustavoalzamora1997@gmail.com>
2023-01-10 09:28:13 -0300

    first commit

---
+++ src/main/java/com/gaav/Attornatus/Teste/Backend/AttornatusTesteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaav.Attornatus.Teste.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusTesteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusTesteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaav/Attornatus/Teste/Backend/AttornatusTesteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaav.Attornatus.Teste.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusTesteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
